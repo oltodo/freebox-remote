bfcc6deebe784368819aa352c4a5858c06e13a00
Nicolas Bazille <oltodo@msn.com>
2024-12-31 14:52:28 +0100

    Introduce PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,43 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA as pwa } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), pwa({
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Freebox Remote Control',
+      short_name: 'FbxRC',
+      description: 'Remote control for Freebox Revolution',
+      theme_color: '#1d232a',
+      icons: [
+        {
+          src: 'pwa-64x64.png',
+          sizes: '64x64',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'maskable-icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
+      ],
+    },
+    devOptions: {
+      enabled: true,
+    },
+  })],
 })
